40a5c4495d1a09da783e2888a4496e5ab264728b
Miguelangeldch <miguelangeldch@gmail.com>
2023-06-05 23:53:39 -0400

    completado CRUD de la API

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,19 +1,50 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task } from './tasks.model';
+import { Task, TaskStatus } from './tasks.model';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
-  @Get()
-  getAllTasks(): Task[] {
-    return this.tasksService.getAllTasks();
-  }
-
   @Post()
   createTask(@Body() createTaskDto: CreateTaskDto): Task {
     return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Get()
+  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    if (Object.keys(filterDto).length) {
+      return this.tasksService.getTasksWithFilter(filterDto);
+    }
+    return this.tasksService.getAllTasks();
+  }
+
+  @Get(':id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Patch(':id/status')
+  updateStatusTask(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus
+  ): Task | string {
+    return this.tasksService.updateStatusTask(id, status);
+  }
+
+  @Delete(':id')
+  deleteTaskById(@Param('id') id: string): string {
+    return this.tasksService.deleteTaskById(id);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,17 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
   // It is good practice to declare variables or constants with a private prop
   // because it prevents other dependencies outside of this class from mutating it.
   private tasks: Task[] = [];
-
-  // The method expose the data
-  getAllTasks(): Task[] {
-    return this.tasks;
-  }
 
   createTask(createTaskDto: CreateTaskDto): Task {
     const { title, description } = createTaskDto;
@@ -24,7 +20,50 @@ export class TasksService {
     };
 
     this.tasks.push(task);
-
     return task;
+  }
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  getTasksWithFilter(filterDto: GetTasksFilterDto): Task[] {
+    const { status, search } = filterDto;
+    let tasks = this.getAllTasks();
+
+    if (status) {
+      tasks = tasks.filter((task) => task.status === status);
+    }
+
+    if (search) {
+      tasks = tasks.filter((task) => {
+        const _search =
+          task.title.includes(search) || task.description.includes(search);
+        return _search;
+      });
+    }
+
+    return tasks;
+  }
+
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  updateStatusTask(id: string, status: TaskStatus): Task | string {
+    const task = this.getTaskById(id);
+    if (!task) return 'Task not found';
+    task.status = status;
+    return task;
+  }
+
+  deleteTaskById(id: string): string {
+    const task = this.tasks.find((task) => task.id === id);
+    if (!task) return 'Task not found';
+
+    const taskIndex = this.tasks.indexOf(task);
+    this.tasks.splice(taskIndex, 1);
+
+    return `The task ${id} has been deleted`;
   }
 }
