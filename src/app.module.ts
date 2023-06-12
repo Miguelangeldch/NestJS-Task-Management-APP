777d6b8608407c0049150fc6e862b01e008f0a59
Miguelangeldch <miguelangeldch@gmail.com>
2023-06-12 13:21:37 -0400

    config enviroment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,21 +4,31 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './tasks/tasks.entity';
 import { AuthModule } from './auth/auth.module';
 import { User } from './auth/user.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ENTITIES_PATH } from './constants';
+import { configValidationSchema } from './config.schema';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+      validationSchema: configValidationSchema
+    }),
     TasksModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'task-management',
-      entities: [ENTITIES_PATH],
-      autoLoadEntities: true,
-      synchronize: true
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DATABASE_HOST'),
+        port: configService.get('DATABASE_PORT'),
+        username: configService.get('DATABASE_USERNAME'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        entities: [ENTITIES_PATH],
+        autoLoadEntities: true,
+        synchronize: true
+      })
     }),
     AuthModule
   ]
--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -5,15 +5,17 @@ import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { JwtPayload } from './jwt-payload.interface';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
     @InjectRepository(User)
-    private usersRepository: Repository<User>
+    private usersRepository: Repository<User>,
+    private configService: ConfigService
   ) {
     super({
-      secretOrKey: 'abc123',
+      secretOrKey: configService.get('JWT_SECRET'),
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
     });
   }
