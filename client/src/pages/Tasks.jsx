78787b8b1e8e1953f19ef370e4684811da2cb167
Miguelangeldch <miguelangeldch@gmail.com>
2023-06-27 10:47:42 -0400

    reorganización de carpetas

--- client/src/pages/Tasks.jsx
+++ client/src/pages/Tasks.jsx
@@ -9,22 +9,30 @@ export default function Tasks () {
   const { data, error, isLoading } = useTasks()
 
   return (
-
     <Grid margin='auto' maxW='80vw' p={3}>
       <Filter />
       {error && <ErrorCard error={error} />}
-      <Stack direction={{ base: 'column', md: 'row' }} flexWrap='wrap' rowGap={5} justify='center'>
+      <Stack
+        direction={{ base: 'column', md: 'row' }}
+        flexWrap='wrap'
+        rowGap={5}
+        justify='center'
+      >
         {!isLoading
-          ? filterTasks(data).map((task) => {
-            return <TaskCard key={task.id} task={task} />
-          })
-          : <Spinner
+          ? (
+              filterTasks(data).map((task) => {
+                return <TaskCard key={task.id} task={task} />
+              })
+            )
+          : (
+            <Spinner
               thickness='4px'
               speed='0.65s'
               emptyColor='gray.200'
               color='blue.500'
               size='xl'
-            />}
+            />
+            )}
       </Stack>
     </Grid>
   )
